b34a796154381f3f72c5c75dba278c7b79d98453
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-13 10:09:59 +0000

    create countries migration

---
+++ database/migrations/2025_03_13_100546_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('capital');
+            $table->integer('population');
+            $table->string('region');
+            $table->string('flag_url')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('language')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
